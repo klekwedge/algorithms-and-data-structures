3bf93a32f810a720a6539e78b160616574cfad55
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-08 14:49:14 +0300

    feat: add the solution to the fifth task

---
+++ Tasks/05.js
@@ -0,0 +1,12 @@
+// Exit the loop by changing only the two marked lines. The result in the console now stops at 9 9. 
+// Should be at 5 4.
+
+for (let i = 0; i < 6; i++) { //! Эту строку можно изменить
+  for (let j = 0; j < 10; j++) {
+    if (i === 5 && j === 5) {
+      break;
+    }
+ 
+    console.log(i, j);
+  }
+ }
