5e4bab90aa3a793b1bc6660721a33688aeebdbf2
Andrey <antonov.andrey.10101@gmail.com>
2023-02-07 17:06:43 +0300

    feat: add a task for the fibonacci method

---
+++ Common Algorithms/Fibonacci numbers/1.js
@@ -0,0 +1,13 @@
+function fibonacchi(n) {
+  if (n == 0) {
+    return 0;
+  }
+
+  if (n == 1) {
+    return 1;
+  }
+  return fibonacchi(n - 2) + fibonacchi(n - 1);
+}
+
+console.log(fibonacchi(10));
+// 55
