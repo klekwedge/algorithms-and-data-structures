975bcf5ded39d358a348c33c79f226dcd2904c56
Andrey <antonov.andrey.10101@gmail.com>
2023-02-07 09:21:39 +0300

    feat: add a task for the sliding window method

---
+++ Common Algorithms/Sliding window method/Subsequence with maximum sum.js
@@ -0,0 +1,41 @@
+function maxSumSubsequence(data, n) {
+  if (!Array.isArray(data)) {
+    return null;
+  }
+
+  if (n === 0) {
+    return null;
+  }
+
+  let bestStart = 0;
+  let bestLength = 1;
+  let maxSum = data[0];
+
+  let curStart = bestStart;
+  let curLength = bestLength;
+  let curSum = maxSum;
+
+  for (let i = 1; i < n; i++) {
+    if (curSum < 0) {
+      curStart = i;
+      curLength = 1;
+      curSum = data[i];
+    } else {
+      curLength += 1;
+      curSum += data[i];
+    }
+
+    if (curSum > maxSum) {
+      bestStart = curStart;
+      bestLength = curLength;
+      maxSum = curSum;
+    }
+  }
+
+  return { bestStart, bestLength, maxSum };
+}
+
+const arr = [2, 5, -8, 3, 9, -2, -4, 0, 2, 6, -3, -5, 0, -4, 2, 3, 1];
+
+console.log(maxSumSubsequence(arr, arr.length));
+// { bestStart: 3, bestLength: 7, maxSum: 14 }
