f9485c43d48b135bdd2f6f0dc4cc125ad6f635ac
Andrey <antonov.andrey.10101@gmail.com>
2023-02-07 19:43:41 +0300

    feat: add a task for the fibonacci method

---
+++ Common Algorithms/Fibonacci numbers/4.js
@@ -0,0 +1,15 @@
+// «Правильная» рекурсия, работающая за линейное время
+
+const fib2 = (n) => {
+  if (n == 0) {
+    return [0, 1];
+  } else {
+    const [prev, next] = fib2(n - 1);
+    return [next, prev + next];
+  }
+};
+
+const fib = (n) => fib2(n)[0];
+
+console.log(fib(10));
+// 55
