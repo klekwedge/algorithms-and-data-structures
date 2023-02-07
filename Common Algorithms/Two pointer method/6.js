c78019b6598058e1d189099c4c10137bf050dafd
Andrey <antonov.andrey.10101@gmail.com>
2023-02-07 16:51:42 +0300

    feat: add a task for the two pointer method

---
+++ Common Algorithms/Two pointer method/6.js
@@ -0,0 +1,31 @@
+// Задача: Дан массив, поверните массив вправо
+// на k шагов, где k неотрицательно. Например,
+// если наш входной массив равен [1, 2, 3, 4, 5, 6, 7]
+// и k равен 4 , то вывод должен быть [4, 5, 6, 7, 1, 2, 3].
+
+function rotate(arr, step) {
+  step %= arr.length;
+  reverse(arr, 0, arr.length - 1);
+  reverse(arr, 0, step - 1);
+  reverse(arr, step, arr.length - 1);
+
+  return arr;
+}
+
+function reverse(arr, start, end) {
+  while (start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start++;
+    end--;
+  }
+
+  return arr;
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(rotate(arr, 4));
+// [
+//     4, 5, 6, 7,
+//     1, 2, 3
+// ]
