8642cba02d4e63a81fe2233b932997154b7ea554
Andrey <antonov.andrey.10101@gmail.com>
2023-02-08 20:09:36 +0300

    feat: add advanced stack

--- Data Structures/Array-like structures/Stack.js
+++ Data Structures/Array-like structures/Stack.js
@@ -1,3 +1,5 @@
+// LIFO (последним пришел – первым вышел)
+
 class LinkedListNode {
   constructor(value, next = null) {
     this.value = value;
@@ -148,9 +150,28 @@ class LinkedList {
   }
 }
 
-const list = new LinkedList();
-list.append(5);
-list.append(3);
-list.append(2);
-list.append(5);
-list.append(7);
+class Stack {
+  constructor() {
+    this.linkedList = new LinkedList();
+  }
+
+  isEmpty() {
+    return !this.linkedList.head;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.linkedList.head.value;
+  }
+
+  push(value) {
+    this.linkedList.prepend(value);
+  }
+
+  pop() {
+    const removedHead = this.linkedList.deleteHead();
+    return removedHead ? removedHead.value : null;
+  }
+}
