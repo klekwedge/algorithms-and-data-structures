1f471be2856c23302a04739fb1fa089d6441ac60
Andrey <antonov.andrey.10101@gmail.com>
2023-02-09 08:46:45 +0300

    feat: add algorithm for create unique array

---
+++ Algorithms/Useful transformations/Practical use/Unique array.js
@@ -0,0 +1,6 @@
+// get uniq from array
+
+const numbers = [1, 2, 1, 1, 2, 1, 3, 4, 1];
+
+const uniq = [...new Set(numbers)]; // => [ 1, 2, 3, 4 ]
+const uniq2 = Array.from(new Set(numbers)); // => [ 1, 2, 3, 4 ]
