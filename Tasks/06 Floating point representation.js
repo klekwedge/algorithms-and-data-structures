6f1d0ccbb00febbfecbebc6c5723b1a62e950b54
Andrey <antonov.andrey.10101@gmail.com>
2023-03-02 08:34:03 +0300

    feat: update structure

--- Tasks/06 Floating point representation.js
+++
@@ -1,7 +0,0 @@
-// An apple costs 1.15, an orange costs 2.30. 
-// How much do they cost together - what is the sum of 1.15 + 2.30 
-// in terms of JavaScript?
-
-console.log(1.15 + 2.30); // 3.4499999999999997
-console.log(1.1 + 2.31); // 3.41
-console.log(1.1 + 2.3); // 3.4
--- Tasks/07 Typeof null.js
+++
@@ -1,6 +0,0 @@
-// What is typeof null in use strict mode?
-
-console.log(typeof null); // object
-
-'use strict'
-console.log(typeof null); // object
