171a2458636d7c59887b0bf2c15e4ba644b4ab5d
Andrey <antonov.andrey.10101@gmail.com>
2023-03-17 18:58:00 +0300

    feat: add task from youtube

---
+++ Examples/03.js
@@ -0,0 +1,23 @@
+function findVowels(str) {
+  let count = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
+
+console.log(findVowels("hello")); // 2
+console.log(findVowels("why")); // 0
+
+function findVowels2(str) {
+  const matched = str.match(/[aeiou]/gi);
+  return matched ? matched.length : 0;
+}
+
+console.log(findVowels2("hello")); // 2
+console.log(findVowels2("why")); // 0
