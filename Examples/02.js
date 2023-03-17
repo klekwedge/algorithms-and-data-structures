559dd9ed5cfc27da069db09caefdf668657f13bb
Andrey <antonov.andrey.10101@gmail.com>
2023-03-17 18:54:54 +0300

    feat: add tasks from youtube

---
+++ Examples/02.js
@@ -0,0 +1,40 @@
+function fizzBuzz(num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz(24);
+
+// 1
+// 2
+// fizz
+// 4
+// buzz
+// fizz
+// 7
+// 8
+// fizz
+// buzz
+// 11
+// fizz
+// 13
+// 14
+// fizzbuzz
+// 16
+// 17
+// fizz
+// 19
+// buzz
+// fizz
+// 22
+// 23
+// fizz
